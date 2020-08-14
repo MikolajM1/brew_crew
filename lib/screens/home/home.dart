49068d5f495ae217e4d3998088f26243995fab81
Mikołaj <mikififi12@gmail.com>
2020-08-14 10:31:31 +0200

    Added displaying data

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -12,6 +12,17 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: Text('bottom sheet'),
+            );
+          });
+    }
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
@@ -30,6 +41,13 @@ class Home extends StatelessWidget {
                 'Log Out',
                 style: TextStyle(color: Colors.white),
               ),
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.settings, color: Colors.white),
+              label: Text('Settings', style: TextStyle(color: Colors.white)),
+              onPressed: () {
+                _showSettingsPanel();
+              },
             )
           ],
         ),
